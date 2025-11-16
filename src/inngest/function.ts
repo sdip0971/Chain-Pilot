dd95438a8867599a4d4ba232c4b6c8c950b5aeef
sdip0971 <shdipanshu4@gmail.com>
2025-11-16 12:06:41 +0530

    Ai providers

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,18 +1,49 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+import { GoogleGenAI } from "@google/genai";
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
+import { anthropic } from "@ai-sdk/anthropic";
+import { createAnthropic } from "@ai-sdk/anthropic";
+const google = new GoogleGenAI({});
+const openAI = createOpenAI({});
+const antrophic = createAnthropic({});
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute-ai" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-        await step.sleep("wait-a-moment", "5s");
-        await step.run("create-workflow",async()=>{
-                return prisma.workflow.create({
-                    data: {
-                    name: "test workflow",
-                    },
-                });
-        })
-   
+    const { steps:openaisteps} = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openAI("gpt-4"),
+        system: "you are a helpful assistant that helps to generate text based on user prompt",
+        prompt: "what is 2+2",
+      }
+    );
+     const { steps:geministeps } = await step.ai.wrap(
+       "gemini-generate-text",
+       generateText,
+       {
+         model: "gemini-2.5-flash  ",
+         system:
+           "you are a helpful assistant that helps to generate text based on user prompt",
+         prompt: "what is 2+2",
+       }
+     );
+       const { steps: anthropicsteps } = await step.ai.wrap(
+         "antrophic-generate-text",
+         generateText,
+         {
+           model: antrophic("claude-sonnet-4-5"),
+           system:
+             "you are a helpful assistant that helps to generate text based on user prompt",
+           prompt: "what is 2+2",
+         }
+       );
+
+    // return the wrapped step results (or use steps as needed)
+    return {openaisteps,geministeps ,anthropicsteps};
   }
 );
