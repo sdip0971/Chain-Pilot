71c54bcd8e303d77f3fbf03f962ca8e67c0bbd57
sdip0971 <shdipanshu4@gmail.com>
2025-11-23 00:52:01 +0530

    Worflow Crud Finished

--- src/hooks/use-workflows.ts
+++ src/hooks/use-workflows.ts
@@ -1,3 +1,4 @@
+
 import { useTRPC } from "@/trpc/client"
 import {  useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query"
 import { Erica_One } from "next/font/google";
@@ -12,14 +13,13 @@ return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
 }
 export default useSuspenseWorkFlows
 export const useCreateWorkflows = ()=>{
-const router = useRouter()
+
 const queryClient = useQueryClient()
 const trpc = useTRPC()
 return useMutation(trpc.workflows.create.mutationOptions({
     onSuccess:(data)=>{
         toast.success(`Workflow "${data.name}" created`)
         queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions());
-        router.push(`/workflows/${data.id}`)
         
     },
     onError :(error)=>{
