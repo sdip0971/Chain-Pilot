0a213fdd3d3497ab06f67ac4833dc334eea15120
sdip0971 <shdipanshu4@gmail.com>
2025-12-27 19:22:12 +0530

    deploy

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,7 +22,7 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
-  socaialProviders: {
+  socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
